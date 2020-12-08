2da2bd65aecb9ff95e8032bc081c5dce6e920fd3
g1st <gintstan@gmail.com>
2020-12-08 17:34:23 +0000

    Add get cleaners route

--- server/api.js
+++ server/api.js
@@ -4,11 +4,24 @@ import { Connection } from "./db";
 
 const router = new Router();
 
-router.get("/", (_, res, next) => {
-	Connection.connect((err) => {
+router.get("/cleaners", (_, res, next) => {
+	Connection.connect((err, pool) => {
 		if (err) {
 			return next(err);
 		}
+
+		pool
+			.query("SELECT * FROM cleaners")
+			.then(({ rows }) => {
+				return res.json({ cleaners: rows });
+			})
+			.catch((e) => {
+				console.error(e);
+				next(e);
+			});
+	});
+});
+
 		res.json({ message: "Hello, world!" });
 	});
 });
