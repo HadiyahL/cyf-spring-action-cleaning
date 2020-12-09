d693c8c515e33fe6ca496d55fc2c9da9d2cb9882
g1st <gintstan@gmail.com>
2020-12-09 08:27:40 +0000

    Remove unnecessary css files

--- client/src/pages/CreateCleaner.js
+++ client/src/pages/CreateCleaner.js
@@ -1,12 +1,11 @@
 import React from "react";
 import { Container } from "reactstrap";
 import { CreateCleanerForm } from "../components";
-import "./CreateCleaner.css";
 
 const CreateCleaner = () => {
 	return (
 		<Container>
-			<h2 className="addcleaner-title mt-4 mt-md-5 mb-5 mb-md-5">
+			<h2 className="text-center mt-4 mt-md-5 mb-5 mb-md-5">
 				Add a new cleaner
 			</h2>
 			<CreateCleanerForm />
