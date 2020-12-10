8f6a49717bfc496b082d433322dc5756725d9fde
Gennady Tabala <gtabala@gmail.com>
2020-12-10 00:56:31 +0000

    eslint errors.

--- client/src/pages/ShowWorkers.js
+++ client/src/pages/ShowWorkers.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Container } from "reactstrap";
 import { ShowWorkersForm } from "../components";
 
-const ShowWorkers = ({kick}) => {
+const ShowWorkers = ({ kick }) => {
 	return (
 		<Container>
 			<h2 className="text-center mt-4 mt-md-5 mb-5 mb-md-5">
