e2b798477b826be84a5eead46e02c13de484c9b6
g1st <gintstan@gmail.com>
2020-12-15 15:19:26 +0000

    Add spinner

--- client/src/components/CreateJob/Modal.js
+++ client/src/components/CreateJob/Modal.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 import {
 	Modal,
 	ModalHeader,
@@ -8,39 +9,52 @@ import {
 	ListGroupItem,
 	Button,
 } from "reactstrap";
+import Spinner from "../UI/Spinner";
 
 const ModalComponent = ({ isOpen, toggle, data }) => {
 	return (
 		<div>
 			<Modal isOpen={isOpen} toggle={toggle}>
-				<ModalHeader toggle={toggle}>Select {data.name}</ModalHeader>
-				<ModalBody>
-					<ListGroup>
-						{data.data.map((item) => {
-							return (
-								<ListGroupItem
-									tag="button"
-									action
-									key={item.id}
-									onClick={() => {
-										data.fetchFunction(item.id);
-										toggle();
-									}}
-								>
-									{item.name}
-								</ListGroupItem>
-							);
-						})}
-					</ListGroup>
-				</ModalBody>
-				<ModalFooter>
-					<Button color="secondary" onClick={toggle}>
-						Close
-					</Button>
-				</ModalFooter>
+				{data ? (
+					<>
+						<ModalHeader toggle={toggle}>Select {data.name}</ModalHeader>
+						<ModalBody>
+							<ListGroup>
+								{data.data.map((item) => {
+									return (
+										<ListGroupItem
+											tag="button"
+											action
+											key={item.id}
+											onClick={() => {
+												data.fetchFunction(item.id);
+												toggle();
+											}}
+										>
+											{item.name || item.address}
+										</ListGroupItem>
+									);
+								})}
+							</ListGroup>
+						</ModalBody>
+						<ModalFooter>
+							<Button color="secondary" onClick={toggle}>
+								Close
+							</Button>
+						</ModalFooter>
+					</>
+				) : (
+					<Spinner />
+				)}
 			</Modal>
 		</div>
 	);
+};
+
+ModalComponent.propTypes = {
+	toggle: PropTypes.func.isRequired,
+	isOpen: PropTypes.bool.isRequired,
+	data: PropTypes.object,
 };
 
 export default ModalComponent;
