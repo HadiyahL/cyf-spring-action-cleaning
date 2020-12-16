bd689eb340d2e94d2e50c37a90f5efb3744c1222
g1st <gintstan@gmail.com>
2020-12-16 15:23:12 +0000

    Show success alert after adding worker

--- client/src/components/CreateWorkerForm.js
+++ client/src/components/CreateWorkerForm.js
@@ -10,6 +10,7 @@ import {
 	Col,
 } from "reactstrap";
 import { postWorkers } from "../service";
+import SuccessAlert from "./UI/SuccessAlert";
 
 const CreateWorkerForm = () => {
 	const [state, setState] = useState({
@@ -21,6 +22,7 @@ const CreateWorkerForm = () => {
 		contract: false,
 	});
 	const [errors, setErrors] = useState({});
+	const [workerAdded, setWorkerAdded] = useState({ state: false, name: "" });
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
@@ -30,6 +32,7 @@ const CreateWorkerForm = () => {
 				if (res.errors) {
 					setErrors(formatErrors(res.errors));
 				} else {
+					setWorkerAdded({ state: true, name: state.name });
 					clearForm();
 				}
 			})
@@ -66,88 +69,97 @@ const CreateWorkerForm = () => {
 	};
 
 	return (
-		<Row className="justify-content-center">
-			<Col xs="12" sm="12" md="8" lg="6" xl="6">
-				<Form onSubmit={handleSubmit}>
-					<FormGroup>
-						<Label for="name">Name</Label>
-						<Input
-							type="text"
-							name="name"
-							id="name"
-							placeholder="Enter full name"
-							onChange={handleChange}
-							value={state.name}
-						/>
-						{errors.name && <FormText color="danger">{errors.name}</FormText>}
-					</FormGroup>
-					<FormGroup>
-						<Label for="email">Email</Label>
-						<Input
-							type="email"
-							name="email"
-							id="email"
-							placeholder="Enter email"
-							onChange={handleChange}
-							value={state.email}
-						/>
-						{errors.email && <FormText color="danger">{errors.email}</FormText>}
-					</FormGroup>
-					<FormGroup>
-						<Label for="address">Address</Label>
-						<Input
-							type="text"
-							name="address"
-							id="address"
-							placeholder="Enter address"
-							onChange={handleChange}
-							value={state.address}
-						/>
-						{errors.address && (
-							<FormText color="danger">{errors.address}</FormText>
-						)}
-					</FormGroup>
-					<FormGroup>
-						<Label for="phone">Phone Number</Label>
-						<Input
-							type="text"
-							name="phone"
-							id="phone"
-							placeholder="Enter phone number"
-							onChange={handleChange}
-							value={state.phone}
-						/>
-						{errors.phone && <FormText color="danger">{errors.phone}</FormText>}
-					</FormGroup>
-					<FormGroup>
-						<Label for="whatsapp">Whatsapp Number</Label>
-						<Input
-							type="text"
-							name="whatsapp"
-							id="whatsapp"
-							placeholder="Enter whatsapp"
-							onChange={handleChange}
-							value={state.whatsapp}
-						/>
-						{errors.whatsapp && (
-							<FormText color="danger">{errors.whatsapp}</FormText>
-						)}
-					</FormGroup>
-					<FormGroup check className="mb-4">
-						<Label check>
+		<>
+			{workerAdded.state && (
+				<SuccessAlert text={`Cleaner ${workerAdded.name} added`} />
+			)}
+			<Row className="justify-content-center">
+				<Col xs="12" sm="12" md="8" lg="6" xl="6">
+					<Form onSubmit={handleSubmit}>
+						<FormGroup>
+							<Label for="name">Name</Label>
 							<Input
-								name="contract"
-								type="checkbox"
+								type="text"
+								name="name"
+								id="name"
+								placeholder="Enter full name"
 								onChange={handleChange}
-								checked={state.contract}
-							/>{" "}
-							Permanent contract
-						</Label>
-					</FormGroup>
-					<Button color="primary">Submit</Button>
-				</Form>
-			</Col>
-		</Row>
+								value={state.name}
+							/>
+							{errors.name && <FormText color="danger">{errors.name}</FormText>}
+						</FormGroup>
+						<FormGroup>
+							<Label for="email">Email</Label>
+							<Input
+								type="email"
+								name="email"
+								id="email"
+								placeholder="Enter email"
+								onChange={handleChange}
+								value={state.email}
+							/>
+							{errors.email && (
+								<FormText color="danger">{errors.email}</FormText>
+							)}
+						</FormGroup>
+						<FormGroup>
+							<Label for="address">Address</Label>
+							<Input
+								type="text"
+								name="address"
+								id="address"
+								placeholder="Enter address"
+								onChange={handleChange}
+								value={state.address}
+							/>
+							{errors.address && (
+								<FormText color="danger">{errors.address}</FormText>
+							)}
+						</FormGroup>
+						<FormGroup>
+							<Label for="phone">Phone Number</Label>
+							<Input
+								type="text"
+								name="phone"
+								id="phone"
+								placeholder="Enter phone number"
+								onChange={handleChange}
+								value={state.phone}
+							/>
+							{errors.phone && (
+								<FormText color="danger">{errors.phone}</FormText>
+							)}
+						</FormGroup>
+						<FormGroup>
+							<Label for="whatsapp">Whatsapp Number</Label>
+							<Input
+								type="text"
+								name="whatsapp"
+								id="whatsapp"
+								placeholder="Enter whatsapp"
+								onChange={handleChange}
+								value={state.whatsapp}
+							/>
+							{errors.whatsapp && (
+								<FormText color="danger">{errors.whatsapp}</FormText>
+							)}
+						</FormGroup>
+						<FormGroup check className="mb-4">
+							<Label check>
+								<Input
+									name="contract"
+									type="checkbox"
+									onChange={handleChange}
+									checked={state.contract}
+								/>{" "}
+								Permanent contract
+							</Label>
+						</FormGroup>
+						<Button color="primary">Submit</Button>
+					</Form>
+				</Col>
+			</Row>
+		</>
 	);
 };
 
