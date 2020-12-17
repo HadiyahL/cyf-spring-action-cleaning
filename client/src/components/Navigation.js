6be159b1d6dd9424955290470b091bb78811de95
Hadiyah <62774630+HadiyahL@users.noreply.github.com>
2020-12-17 21:32:37 +0000

    added logo and some styling to the navbar

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -6,8 +6,9 @@ import {
 	NavbarBrand,
 	Nav,
 	NavItem,
-	NavLink,
 } from "reactstrap";
+import { Link } from "react-router-dom";
+import  logo  from "../assets/logo.png";
 
 const Navigation = () => {
 	const [isOpen, setIsOpen] = useState(false);
@@ -17,18 +18,30 @@ const Navigation = () => {
 	return (
 		<div>
 			<Navbar color="light" light expand="md">
-				<NavbarBrand href="/">Home</NavbarBrand>
+				<NavbarBrand href="/">
+					<img
+						src={logo}
+						style={{ height: 50, width: 60, marginTop: -7 }}
+						alt="spring-action-logo"
+					/>
+				</NavbarBrand>
 				<NavbarToggler onClick={toggle} />
 				<Collapse isOpen={isOpen} navbar>
-					<Nav className="mr-auto" navbar>
-						<NavItem>
-							<NavLink href="/customers">Client</NavLink>
+					<Nav className="ml-auto d-flex justify-content-end" navbar>
+						<NavItem className="mr-5 pb-2">
+							<Link className="text-decoration-none" to="/customers">
+								Client{" "}
+							</Link>
 						</NavItem>
-						<NavItem>
-							<NavLink href="/workers">Cleaners</NavLink>
+						<NavItem className="mr-5 pb-2">
+							<Link className="text-decoration-none" to="/workers">
+								Cleaners{" "}
+							</Link>
 						</NavItem>
-						<NavItem>
-							<NavLink href="/jobs">Jobs</NavLink>
+						<NavItem className="mr-5 pb-2">
+							<Link className="text-decoration-none" to="/jobs">
+								Jobs
+							</Link>
 						</NavItem>
 					</Nav>
 				</Collapse>
