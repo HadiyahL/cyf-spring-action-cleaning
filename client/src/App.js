e671106ca0d7e1a5e7805189b0e3239cc4279f16
g1st <gintstan@gmail.com>
2020-12-18 19:35:27 +0000

    Add button for adding new clients, workers and jobs

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import { Navigation } from "./components";
 import {
@@ -18,13 +18,6 @@ export function App() {
 	return (
 		<Router>
 			<Navigation />
-			<main role="main">
-				<Link to="add-worker">Create cleaner</Link>
-				<br />
-				<Link to="add-customer">Create Customer</Link>
-				<br />
-				<Link to="create-job">Create Job</Link>
-			</main>
 			<Switch>
 				<Route path="/add-worker">
 					<CreateWorker />
@@ -36,13 +29,13 @@ export function App() {
 					<EditCustomer />
 				</Route>
 				<Route path="/customers">
-					<Customers  />
+					<Customers />
 				</Route>
 				<Route path="/jobs">
 					<Jobs />
 				</Route>
 				<Route path="/workers">
-					<ShowWorkers  />
+					<ShowWorkers />
 				</Route>
 				<Route path="/edit-worker/:id">
 					<EditWorker />
--- client/src/pages/ShowWorkers.js
+++ client/src/pages/ShowWorkers.js
@@ -1,12 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Container } from "reactstrap";
-import { ShowWorkersTable } from "../components";
+import { ShowWorkersTable, AddNewButton } from "../components";
 
 const ShowWorkers = ({ trigger }) => {
 	return (
 		<Container>
 			<h2 className="text-center mt-4 mt-md-5 mb-5 mb-md-5">Cleaners</h2>
+			<AddNewButton pathname="/add-worker" />
 			<ShowWorkersTable trigger={trigger} />
 		</Container>
 	);
