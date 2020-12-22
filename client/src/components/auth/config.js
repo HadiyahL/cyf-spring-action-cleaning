a244c544145336da124d4ef78625c16715ea2efd
g1st <gintstan@gmail.com>
2020-12-22 16:28:25 +0000

    Add auth config

---
+++ client/src/components/auth/config.js
@@ -0,0 +1,7 @@
+const config = {
+	domain: "dev-dfrupp50.eu.auth0.com",
+	clientId: "HGL1d3oYXtHMqKkTzsG51eX37eGE5A7k",
+	roleUrl: "https://springactioncleaning/roles",
+};
+
+export default config;
