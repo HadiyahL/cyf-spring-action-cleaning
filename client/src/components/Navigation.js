16352dea5fb8514fea1d7340c5b46b867fcaa728
g1st <gintstan@gmail.com>
2020-12-22 16:28:00 +0000

    Hide nav items from unauthenticated users

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 import {
 	Collapse,
 	Navbar,
@@ -8,12 +9,18 @@ import {
 	NavItem,
 } from "reactstrap";
 import { Link } from "react-router-dom";
+import { LoginButton, LogoutButton } from "./auth";
 import logo from "../assets/logo.png";
+import Spinner from "./UI/Spinner";
 
 const Navigation = () => {
 	const [isOpen, setIsOpen] = useState(false);
-
+	const { isLoading, isAuthenticated } = useAuth0();
 	const toggle = () => setIsOpen(!isOpen);
+
+	if (isLoading) {
+		return <Spinner />;
+	}
 
 	return (
 		<div>
@@ -24,30 +31,37 @@ const Navigation = () => {
 				<NavbarToggler onClick={toggle} />
 				<Collapse isOpen={isOpen} navbar>
 					<Nav className="ml-auto d-flex justify-content-end navItems" navbar>
-						<NavItem className="mr-md-5 pb-2 pt-2 pb-md-0 pt-md-0 text-center">
-							<Link
-								className="text-decoration-none link text-secondary "
-								to="/customers"
-							>
-								Client{" "}
-							</Link>
-						</NavItem>
-						<NavItem className="mr-md-5 pb-2 pt-2 pb-md-0 pt-md-0 text-center">
-							<Link
-								className="text-decoration-none link text-secondary "
-								to="/workers"
-							>
-								Cleaners{" "}
-							</Link>
-						</NavItem>
-						<NavItem className="mr-md-5 pb-2 pt-2 pb-md-0 pt-md-0 text-center">
-							<Link
-								className="text-decoration-none link text-secondary "
-								to="/jobs"
-							>
-								Jobs
-							</Link>
-						</NavItem>
+						{isAuthenticated ? (
+							<>
+								<NavItem className="mr-md-5 pb-2 pt-2 pb-md-0 pt-md-0 text-center d-flex align-items-center">
+									<Link
+										className="text-decoration-none link text-secondary "
+										to="/customers"
+									>
+										Client{" "}
+									</Link>
+								</NavItem>
+								<NavItem className="mr-md-5 pb-2 pt-2 pb-md-0 pt-md-0 text-center d-flex align-items-center">
+									<Link
+										className="text-decoration-none link text-secondary "
+										to="/workers"
+									>
+										Cleaners{" "}
+									</Link>
+								</NavItem>
+								<NavItem className="mr-md-5 pb-2 pt-2 pb-md-0 pt-md-0 text-center d-flex align-items-center">
+									<Link
+										className="text-decoration-none link text-secondary "
+										to="/jobs"
+									>
+										Jobs
+									</Link>
+								</NavItem>
+								<LogoutButton />
+							</>
+						) : (
+							<LoginButton />
+						)}
 					</Nav>
 				</Collapse>
 			</Navbar>
