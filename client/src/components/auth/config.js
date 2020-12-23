5e1dae4df532dc62dee0c5c669becb196d287aa6
g1st <gintstan@gmail.com>
2020-12-23 11:15:27 +0000

    Add audience name

--- client/src/components/auth/config.js
+++ client/src/components/auth/config.js
@@ -2,6 +2,7 @@ const config = {
 	domain: "dev-dfrupp50.eu.auth0.com",
 	clientId: "HGL1d3oYXtHMqKkTzsG51eX37eGE5A7k",
 	roleUrl: "https://springactioncleaning/roles",
+	audience: "https://springactioncleaning/",
 };
 
 export default config;
