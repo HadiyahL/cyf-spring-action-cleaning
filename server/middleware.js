4ffb47981345423181c2e30a6f0a99f24e3a2487
g1st <gintstan@gmail.com>
2020-12-23 11:08:36 +0000

    Add check if user is authenticated

--- server/middleware.js
+++ server/middleware.js
@@ -1,4 +1,6 @@
 import path from "path";
+import jwt from "express-jwt";
+import jwksRsa from "jwks-rsa";
 
 export const httpsOnly = () => (req, res, next) => {
 	if (!req.secure) {
@@ -21,3 +23,24 @@ export const pushStateRouting = (apiRoot, staticDir) => (req, res, next) => {
 	}
 	next();
 };
+
+// Authorization middleware. When used, the
+// Access Token must exist and be verified against
+// the Auth0 JSON Web Key Set
+export const checkAuth = jwt({
+	// Dynamically provide a signing key
+	// based on the kid in the header and
+	// the signing keys provided by the JWKS endpoint.
+	secret: jwksRsa.expressJwtSecret({
+		cache: true,
+		rateLimit: true,
+		jwksRequestsPerMinute: 5,
+		jwksUri: "https://dev-dfrupp50.eu.auth0.com/.well-known/jwks.json",
+	}),
+
+	// Validate the audience and the issuer.
+	audience: "https://springactioncleaning/",
+	issuer: "https://dev-dfrupp50.eu.auth0.com/",
+	algorithms: ["RS256"],
+});
+
