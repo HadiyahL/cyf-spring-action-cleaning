3a41ce9b238d8b43b9680371f47a234b6c1d8236
Gennady Tabala <gtabala@gmail.com>
2020-12-29 13:38:35 +0000

    Test

--- client/src/components/Reports/ResultPage.js
+++ client/src/components/Reports/ResultPage.js
@@ -9,25 +9,22 @@ import ResultTableBody from "./ResultTableBody";
 const ResultPage = () => {
 
 	const { id, start, finish, name } = useParams();
-	// const [state, setState]=useState(useFetch(`/reports/worker_total/${id}/${start}/${finish}`))
-	// const { total_data, total_error, total_isLoading } = useFetch(`/reports/worker/${id}/${start}/${finish}`);
 	const { data, error, isLoading } = useFetch(`/reports/worker/${id}/${start}/${finish}`);
 	const total_data = useFetch(`/reports/worker_total/${id}/${start}/${finish}`);
 	
 
-	if (total_data.error) {
+	if (total_data.error || error) {
 		return <div>Error</div>;
-	} else if (total_data.isLoading ) {
+	} else if (total_data.isLoading || isLoading ) {
 		return <Spinner />;
 	} else if (total_data.data) {
-		console.log(data,"  ",total_data.data);
 		return (
 			<Container>
 				<h2 className="text-center mt-4 mt-md-5 mb-5 mb-md-5">
 					{name}
 				</h2>
 				<h3 className="text-center mt-4 mt-md-5 mb-5 mb-md-5">
-					{"From: "+start+" To: "+finish}
+					{"Work duration from: "+start+" to: "+finish}
 				</h3>
 				<Table striped hover responsive>
 					<ResultTableHead labels={["Customer", "Address", "Duration"]} />
--- server/routes/reports.js
+++ server/routes/reports.js
@@ -5,9 +5,9 @@ import db from "../db";
 const router = new Router();
 
 router.get("/reports/customer/:id", (req, res, next) => {
-    const {id} = req.params;
+	const { id } = req.params;
 	const { start_date, finish_date } = req.body;
-    db.query(`SELECT  w.name, SUM((j.end_time-j.start_time)*j.pay_rate) pay, SUM(j.end_time - j.start_time) duration 
+	db.query(`SELECT  w.name, SUM((j.end_time-j.start_time)*j.pay_rate) pay, SUM(j.end_time - j.start_time) duration 
     FROM jobs j INNER JOIN branches b ON j.branch_id=b.id INNER JOIN workers w ON j.worker_id=w.id 
     WHERE j.customer_id=$1 AND j.visit_on BETWEEN  $2 AND $3 GROUP BY w.name`,[id, start_date, finish_date])
 		.then(({ rows }) => {
@@ -27,7 +27,7 @@ router.get("/reports/worker/:worker_id/:start/:finish", (req, res, next) => {
 		`SELECT  c.name, b.address, SUM(j.end_time - j.start_time) duration 
 	FROM jobs j INNER JOIN workers w ON j.worker_id=w.id INNER JOIN branches b ON j.branch_id=b.id 
 	INNER JOIN customers c ON j.customer_id=c.id 
-    WHERE w.id=$1 AND j.visit_on BETWEEN  $2 AND $3 GROUP BY (b.address, c.name)`,[worker_id, start, finish]
+    WHERE w.id=$1 AND j.visit_on BETWEEN  $2 AND $3 GROUP BY (b.address, c.name) ORDER BY c.name`,[worker_id, start, finish]
 	)
 		.then(({ rows }) => {
 			return res.json({ rows });
@@ -42,13 +42,13 @@ router.get("/reports/worker_total/:worker_id/:start/:finish", (req, res, next) =
 	const { worker_id, start, finish } = req.params;
 
 	db.query(
-		`SELECT  w.name address, SUM(j.end_time - j.start_time) duration 
+		`SELECT  w.id , SUM(j.end_time - j.start_time) duration 
 	FROM jobs j INNER JOIN workers w ON j.worker_id=w.id 
-    WHERE w.id=$1 AND j.visit_on BETWEEN  $2 AND $3 GROUP BY (w.name)`,[worker_id, start, finish]
+    WHERE w.id=$1 AND j.visit_on BETWEEN  $2 AND $3 GROUP BY (w.id)`,[worker_id, start, finish]
 	)
-		.then(({rows}) => {
+		.then(({ rows }) => {
 			// if(!rows === true) {rows[0].address="Total duration:"}
-			return res.json({rows});
+			return res.json({ rows });
 		})
 		.catch((e) => {
 			console.error(e);
