7e527befdf59d8f2cb1af6e0aad9f240719ea19a
g1st <gintstan@gmail.com>
2021-01-04 14:03:16 +0000

    Add reactstrap components

--- client/src/components/Jobs/DateFilter.js
+++ client/src/components/Jobs/DateFilter.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { FormGroup, Label, Input } from "reactstrap";
 
 const DateFilter = ({ state, setState }) => {
 	const handleDateChange = (e) => {
@@ -13,28 +14,32 @@ const DateFilter = ({ state, setState }) => {
 
 	return (
 		<div className="mb-1">
-			<label htmlFor="start_time" className="mr-3">
-				<div className="time-input-label">From: </div>
-				<input
-					type="date"
-					id="start_time"
-					name="start_time"
-					value={start_time}
-					onChange={handleDateChange}
-					className="ml-1"
-				/>
-			</label>
-			<label htmlFor="end_time" className="ml-sm-4">
-				<div className="time-input-label">To: </div>
-				<input
-					type="date"
-					id="end_time"
-					name="end_time"
-					value={end_time}
-					onChange={handleDateChange}
-					className="ml-1"
-				/>
-			</label>
+			<FormGroup className="d-inline-block">
+				<Label htmlFor="start_time" className="mr-3 d-flex">
+					<div className="align-self-center">From: </div>
+					<Input
+						type="date"
+						id="start_time"
+						name="start_time"
+						value={start_time}
+						onChange={handleDateChange}
+						className="ml-1"
+					/>
+				</Label>
+			</FormGroup>
+			<FormGroup className="d-inline-block">
+				<Label htmlFor="end_time" className="d-flex">
+					<div className="align-self-center">To: </div>
+					<Input
+						type="date"
+						id="end_time"
+						name="end_time"
+						value={end_time}
+						onChange={handleDateChange}
+						className="ml-1"
+					/>
+				</Label>
+			</FormGroup>
 		</div>
 	);
 };
