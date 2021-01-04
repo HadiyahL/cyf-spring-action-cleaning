344aa9a52678377890c34e828a12ce94b77dfee0
g1st <gintstan@gmail.com>
2021-01-04 14:07:02 +0000

    Add page for recreating jobs

--- client/src/pages/Recurring.js
+++ client/src/pages/Recurring.js
@@ -1,53 +1,91 @@
-import React, { useState } from "react";
-import { DateTime } from "luxon";
-import { getJobs } from "../service";
-import useFetch from "../hooks/useFetch";
+import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+import { getJobs, postBatchOfJobs } from "../service";
+import { Table, Button, Container } from "reactstrap";
+import DateFilter from "../components/Jobs/DateFilter";
+import {
+	getWeekFromDate,
+	sortByField,
+	addDaysToCleaningTime,
+} from "../util/helpers";
+import { Spinner } from "../components";
+import { RecurringJobsTableHead, RecurringJobsTableBody } from "../components";
+import useAuthorizationHeaders from "../hooks/useAuthorizationHeaders";
 
 const Recurring = () => {
-	// const { loading, data, error } = useFetch("/jobs/range", data);
-	const [jobs, setJobs] = useState([]);
-	const getWeekFromDate = (date = new Date()) => {
-		const startOfWeek = DateTime.fromJSDate(date)
-			.minus({ days: 7 })
-			.startOf("week")
-			.toISODate();
-		const endOfWeek = DateTime.fromJSDate(date)
-			.minus({ days: 7 })
-			.endOf("week")
-			.toISODate();
+	const [state, setState] = useState({
+		start_time: getWeekFromDate().start,
+		end_time: getWeekFromDate().end,
+		jobs: [],
+		isLoading: true,
+		error: null,
+	});
+	const history = useHistory();
+	const authorizationHeaders = useAuthorizationHeaders();
 
-		return {
-			startOfWeek,
-			endOfWeek,
+	useEffect(() => {
+		let isActive = true;
+
+		getJobs(state.start_time, state.end_time)
+			.then((data) => {
+				if (isActive) {
+					setState((state) => ({
+						...state,
+						isLoading: false,
+						error: null,
+						jobs: addDaysToCleaningTime(data.jobs, 14),
+					}));
+				}
+			})
+			.catch((error) => {
+				setState((state) => ({
+					...state,
+					isLoading: false,
+					error: error.message,
+				}));
+			});
+
+		return () => {
+			isActive = false;
 		};
-	};
+	}, [state.start_time, state.end_time]);
 
 	const handleClick = () => {
-		getJobs(getWeekFromDate().startOfWeek, getWeekFromDate().endOfWeek)
-			.then((res) => {
-				console.log("res.jobs :>> ", res.jobs);
-				setJobs(res.jobs);
-			})
+		postBatchOfJobs(state, authorizationHeaders)
+			.then(() => history.push("/jobs"))
 			.catch((e) => console.log(e));
 	};
 
-	return (
-		<div>
-			<div>weeks range below</div>
-			<div>week start: {getWeekFromDate().startOfWeek}</div>
-			<div>week end: {getWeekFromDate().endOfWeek}</div>
-			<div>all jobs for that range below</div>
-			<button onClick={handleClick}>on this button click</button>
-			<ul>
-				{jobs.map((job) => (
-					<li key={job.id}>
-						{job.visit_on.split("T")[0]} {job.address} {job.customer}{" "}
-						{job.worker}
-					</li>
-				))}
-			</ul>
-		</div>
-	);
+	const { error, isLoading, jobs } = state;
+	const sortedJobsByDate = sortByField(jobs, "customer", true);
+
+	if (error) {
+		return <div>Oops, something wrong</div>;
+	} else if (isLoading) {
+		return <Spinner />;
+	} else {
+		return (
+			<Container className="pr-lg-5 pl-lg-5 jobs">
+				<h2 className="text-center mt-4 mt-md-5 mb-5 mb-md-5">
+					Recreate from previous jobs
+				</h2>
+				<DateFilter state={state} setState={setState} />
+				<Table striped hover responsive className="recurring-table">
+					<RecurringJobsTableHead />
+					<RecurringJobsTableBody
+						data={sortedJobsByDate}
+						state={state}
+						setState={setState}
+					/>
+				</Table>
+				<div className="d-flex justify-content-end mb-4">
+					<Button onClick={handleClick} color="success">
+						Create {jobs.length} jobs
+					</Button>
+				</div>
+			</Container>
+		);
+	}
 };
 
 export default Recurring;
