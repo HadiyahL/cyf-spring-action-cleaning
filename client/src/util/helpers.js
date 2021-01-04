6c4b393a8348401d3a52d7c51b7ded97b18ac855
g1st <gintstan@gmail.com>
2021-01-04 14:05:47 +0000

    Add time helper functions

--- client/src/util/helpers.js
+++ client/src/util/helpers.js
@@ -1,3 +1,5 @@
+import { DateTime } from "luxon";
+
 // sort array of objects for specified field
 export const sortDescByABC = (array, field) => {
 	return array.sort((first, second) => {
@@ -25,3 +27,45 @@ export const sortByField = (array, field, isAscending) => {
 		}
 	});
 };
+
+export const getWeekFromDate = (date = new Date()) => {
+	const start = DateTime.fromJSDate(date)
+		.minus({ days: 7 })
+		.startOf("week")
+		.toISODate();
+	const end = DateTime.fromJSDate(date)
+		.minus({ days: 7 })
+		.endOf("week")
+		.toISODate();
+
+	return {
+		start,
+		end,
+	};
+};
+
+export const addDaysToCleaningTime = (jobs, days) =>
+	jobs.map((job) => ({
+		...job,
+		visit_on: DateTime.fromISO(job.visit_on).plus({ days }).toISODate(),
+	}));
+
+export const plusDayForJob = (jobs, id) =>
+	jobs.map((job) => {
+		if (job.id === id) {
+			job.visit_on = DateTime.fromISO(job.visit_on)
+				.plus({ days: 1 })
+				.toISODate();
+		}
+		return job;
+	});
+
+export const minusDayForJob = (jobs, id) =>
+	jobs.map((job) => {
+		if (job.id === id) {
+			job.visit_on = DateTime.fromISO(job.visit_on)
+				.minus({ days: 1 })
+				.toISODate();
+		}
+		return job;
+	});
