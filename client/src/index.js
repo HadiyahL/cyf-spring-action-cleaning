a6157b25eca1dda63f440c46eb4daa0f6e94df2b
g1st <gintstan@gmail.com>
2021-01-05 11:03:32 +0000

    Use index.scss instead of App.css

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Auth0ProviderWithHistory } from "./components/auth";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "./scss/index.scss";
 import App from "./App";
 
 ReactDOM.render(
