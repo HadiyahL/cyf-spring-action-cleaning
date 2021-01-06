fa8d0fff05d06ab9bc12e234f29b191872e8a82c
g1st <gintstan@gmail.com>
2021-01-06 16:46:38 +0000

    Temporary information for spring action team

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import Jobs from "./Jobs";
+import { Container } from "reactstrap";
 
 const HomePage = () => {
 	const { isAuthenticated } = useAuth0();
@@ -9,10 +10,17 @@ const HomePage = () => {
 		return <Jobs />;
 	} else {
 		return (
-			<div className="h1">
-				Create something for this homepage hero section for not authenticated
-				visitors.
-			</div>
+			<Container className="mt-5">
+				<h1 className="mb-5">Spring Action Cleaning</h1>
+				<p>
+					Currently data can be viewed by anyone on the Internet, so do not add
+					any sensitive data yet.
+				</p>
+				<p>
+					All data entered into the database will be wiped out before actual
+					release.
+				</p>
+			</Container>
 		);
 	}
 };
