1ac2040d3e3abf698e8f7df34b277514b728f597
g1st <gintstan@gmail.com>
2021-01-10 12:27:02 +0000

    Add routes to get all jobs and single job by id for a worker

--- server/routes/workers.js
+++ server/routes/workers.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { body, validationResult } from "express-validator";
 import { PhoneNumberUtil } from "google-libphonenumber";
 import db from "../db";
+import { formatWorkerJobs } from "../util/formatJobs";
 import { checkAuth, checkPermission } from "../middleware";
 
 const router = new Router();
@@ -24,6 +25,30 @@ router.get(
 );
 
 router.get(
+	"/workers/jobs",
+	checkAuth,
+	checkPermission("get:workers/jobs"),
+	(req, res, next) => {
+		db.query(
+			`SELECT j.id, b.address, j.visit_on, j.visit_time
+				FROM jobs j
+				INNER JOIN branches b ON j.branch_id=b.id
+				INNER JOIN workers w ON w.id=j.worker_id
+				WHERE w.email=$1
+			`,
+			[req.user["https://springactioncleaning/email"]]
+		)
+			.then(({ rows }) => {
+				return res.json({ jobs: formatWorkerJobs(rows) });
+			})
+			.catch((e) => {
+				console.error(e);
+				next(e);
+			});
+	}
+);
+
+router.get(
 	"/workers/:id",
 	checkAuth,
 	checkPermission("get:workersById"),
@@ -33,6 +58,34 @@ router.get(
 		db.query("SELECT * FROM workers WHERE id=$1", [id])
 			.then(({ rows }) => {
 				return res.json({ workers: rows });
+			})
+			.catch((e) => {
+				console.error(e);
+				next(e);
+			});
+	}
+);
+
+router.get(
+	"/workers/job/:id",
+	checkAuth,
+	checkPermission("get:workers/job/:id"),
+	(req, res, next) => {
+		const { id } = req.params;
+		const email = req.user["https://springactioncleaning/email"];
+
+		db.query(
+			`SELECT b.address, b.contact_name, b.contact_phone, b.details branch_details, j.*, c.name customer, w.name worker, w.email
+				FROM jobs j
+				INNER JOIN branches b ON j.branch_id=b.id
+				INNER JOIN customers c ON j.customer_id=c.id
+				INNER JOIN workers w ON w.id=j.worker_id
+				WHERE j.id=$1 AND w.email=$2
+			`,
+			[id, email]
+		)
+			.then(({ rows }) => {
+				return res.json({ jobs: formatWorkerJobs(rows) });
 			})
 			.catch((e) => {
 				console.error(e);
