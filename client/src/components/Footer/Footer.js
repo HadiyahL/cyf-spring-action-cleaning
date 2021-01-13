9fc1860929d27325875d114a21773f572ef44fe5
Hadiyah <62774630+HadiyahL@users.noreply.github.com>
2021-01-13 23:20:47 +0000

    Added icons to the footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,30 +1,42 @@
 import React from "react";
 import { Container, List } from "reactstrap";
+import { IconContext } from "react-icons/lib";
+import { MdEmail, MdLocationOn, MdPhoneInTalk } from "react-icons/md";
+import { BsFillHouseFill } from "react-icons/bs";
 
 const Footer = () => {
 	return (
 		<footer className="footer mt-auto py-4 bg-light">
 			<Container>
-				<List
-					type="unstyled"
-					className="footer-list mb-0 text-muted text-center"
-				>
-					<li>
-						<a
-							href="mailto:info@springactioncleaning.co.uk"
-							className="text-muted"
-						>
-							info@springactioncleaning.co.uk
-						</a>
-					</li>
-					<li>07799628095</li>
-					<li>Norton House</li>
-					<li>Bird Street</li>
-					<li>Coventry, CV1 5FX</li>
-					<li className="mt-3">
-						A Social enterprise founded by Coventry Refugee and Migrant Centre
-					</li>
-				</List>
+				<IconContext.Provider value={{ className: "react-icons1" }}>
+					<List
+						type="unstyled"
+						className="footer-list mb-0 text-muted text-center"
+					>
+						<li>
+							<a
+								href="mailto:info@springactioncleaning.co.uk"
+								className="text-muted"
+							>
+								<MdEmail />
+								  info@springactioncleaning.co.uk
+							</a>
+						</li>
+						<li>
+							<MdPhoneInTalk /> 07799628095
+						</li>
+						<li>
+							<BsFillHouseFill /> Norton House
+						</li>
+						<li>
+							<MdLocationOn />
+							Bird Street, Coventry, CV1 5FX
+						</li>
+						<li className="mt-3">
+							A Social enterprise founded by Coventry Refugee and Migrant Centre
+						</li>
+					</List>
+				</IconContext.Provider>
 			</Container>
 		</footer>
 	);
