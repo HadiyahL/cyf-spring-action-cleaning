8555970057df60e767aa4f5d861e8235d7718d5b
g1st <gintstan@gmail.com>
2021-02-08 11:39:01 +0000

    Add lazy loading for all routes components

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,15 +1,20 @@
-export { default as CreateWorker } from "./CreateWorker.js";
-export { default as CreateCustomer } from "./CreateCustomer.js";
-export { default as EditCustomer } from "./EditCustomer.js";
-export { default as Customers } from "./Customers.js";
-export { default as ShowWorkers } from "./ShowWorkers.js";
-export { default as Jobs } from "./Jobs.js";
-export { default as CreateJob } from "./CreateJob.js";
-export { default as EditJob } from "./EditJob.js";
-export { default as EditWorker } from "./EditWorker";
-export { default as Recurring } from "./Recurring";
-export { default as WorkerReports } from "./WorkerReports";
-export { default as CustomerReports } from "./CustomerReports";
-export { default as WorkerJobs } from "./WorkerJobs";
-export { default as WorkerJobPage } from "./WorkerJobPage";
-export { default as HomePage } from "./HomePage";
+import { lazy } from "react";
+
+export const CreateWorker = lazy(() => import("./CreateWorker"));
+export const CreateCustomer = lazy(() => import("./CreateCustomer"));
+export const EditCustomer = lazy(() => import("./EditCustomer"));
+export const Customers = lazy(() => import("./Customers"));
+export const ShowWorkers = lazy(() => import("./ShowWorkers"));
+export const Jobs = lazy(() => import("./Jobs"));
+export const CreateJob = lazy(() => import("./CreateJob"));
+export const EditJob = lazy(() => import("./EditJob"));
+export const EditWorker = lazy(() => import("./EditWorker"));
+export const Recurring = lazy(() => import("./Recurring"));
+export const WorkerReports = lazy(() => import("./WorkerReports"));
+export const CustomerReports = lazy(() => import("./CustomerReports"));
+export const WorkerJobs = lazy(() => import("./WorkerJobs"));
+export const WorkerJobPage = lazy(() => import("./WorkerJobPage"));
+export const HomePage = lazy(() => import("./HomePage"));
+export const ResultPage = lazy(() =>
+	import("../components/Reports/ResultPage")
+);
