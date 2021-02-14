8032d1585ef674292c27bcd55122639200cc8d0a
Gennady Tabala <gtabala@gmail.com>
2021-02-14 20:26:41 +0000

    Reports explanation.

--- client/src/components/Reports/ResultTableBody.js
+++ client/src/components/Reports/ResultTableBody.js
@@ -1,27 +1,55 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { useHistory } from "react-router-dom";
 import { formatDate } from "../../util/helpers";
 
+
 const ResultTableBody = ({ data, detailed, tableFooter }) => {
+
+	const history = useHistory();
+
 	const formatDuration = (h = 0, m = 0) => {
 		return ("00" + h).slice(-2) + ":" + ("00" + m).slice(-2);
 	};
+
+	const handleClick = (id) => {
+		history.push(`/edit-jobs/${id}`);
+	};
+
+	const handleKeyPress = (id, e) => {
+		if (e.key === "Enter" && e.target.tagName === "TR") {
+			history.push(`/edit-jobs/${id}`);
+		}
+	};
+
 	if (detailed){
 		return (
 			<tbody>
-				{data.map((line, ind) => (
-					<tr key={ind}>
-						<th scope="row">{line.visit_on ? formatDate(line.visit_on, {
+				{data.map(({
+					id,
+					visit_on,
+					column_1,
+					column_2,
+					duration,
+				}) => (
+					<tr
+						key={id}
+						role="button"
+						onClick={() => handleClick(id)}
+						onKeyPress={(e) => handleKeyPress(id, e)}
+						tabIndex={0}
+					>
+						<th scope="row">{visit_on ? formatDate(visit_on, {
 							year: "numeric",
 							month: "numeric",
 							day: "numeric",
 						}):""}</th>
-						<td>{line.column_1}</td>
+						<td>{column_1}</td>
 						<td className={tableFooter && "font-weight-bold text-right"}>
-							{tableFooter ? "Total duration:" : line.column_2}
+							{tableFooter ? "Total duration:" : column_2}
 						</td>
 						<td className={tableFooter && "font-weight-bold"}>
-							{formatDuration(line.duration.hours, line.duration.minutes)}
+							{formatDuration(duration.hours, duration.minutes)}
 						</td>
 					</tr>
 				))}
@@ -30,14 +58,14 @@ const ResultTableBody = ({ data, detailed, tableFooter }) => {
 	} else {
 		return (
 			<tbody>
-				{data.map((line, ind) => (
+				{data.map(({ column_1, column_2, duration }, ind) => (
 					<tr key={ind}>
-						<th scope="row">{line.column_1}</th>
+						<th scope="row">{column_1}</th>
 						<td className={tableFooter && "font-weight-bold text-right"}>
-							{tableFooter ? "Total duration:" : line.column_2}
+							{tableFooter ? "Total duration:" : column_2}
 						</td>
 						<td className={tableFooter && "font-weight-bold"}>
-							{formatDuration(line.duration.hours, line.duration.minutes)}
+							{formatDuration(duration.hours, duration.minutes)}
 						</td>
 					</tr>
 				))}
