3d0ee8600036c09452c1bf12a5115e7ccad2c683
g1st <gintstan@gmail.com>
2021-03-11 09:18:17 +0000

    Render component based on selected grouping type

--- client/src/components/Reports/General/GeneralReportResultPage.js
+++ client/src/components/Reports/General/GeneralReportResultPage.js
@@ -3,48 +3,61 @@ import { Container } from "reactstrap";
 import { Spinner, Title, BackButton } from "../../index";
 import PropTypes from "prop-types";
 import useFetch from "../../../hooks/useFetch";
-import GeneralReportByCleaner from "./GeneralReportByCleaner";
+import GeneralReportByDate from "./GeneralReportByDate";
 import GeneralReportByCustomer from "./GeneralReportByCustomer";
+import GeneralReportByWorker from "./GeneralReportByWorker";
 
 const GeneralReportResultPage = ({ state, setState }) => {
-	const { start_date, finish_date, byCustomer } = state;
-
+	const { start_date, finish_date, groupBy } = state;
 	const { data, error, isLoading } = useFetch(
 		`/general_reports/general/${start_date}/${finish_date}`
 	);
 
+	let generalReportComponent;
 	if (error) {
 		return <div>Error</div>;
 	} else if (isLoading) {
 		return <Spinner />;
 	} else {
-		return (
-			<Container>
-				<Title text="General report" />
-				<h3 className="text-center mt-4 mt-md-5 mb-5 mb-md-5">
-					Work duration from {start_date} to {finish_date}
-				</h3>
-				{data.generalData.length === 0 ? (
-					<p>No data for this period.</p>
-				) : byCustomer ? (
-					<GeneralReportByCustomer
-						state={state}
-						setState={setState}
-						data={data}
-					/>
-				) : (
-					<GeneralReportByCleaner
-						state={state}
-						setState={setState}
-						data={data}
-					/>
-				)}
-				<div className="d-flex justify-content-end mt-5">
-					<BackButton state={state} setState={setState} />
-				</div>
-			</Container>
-		);
+		if (groupBy === "customer") {
+			generalReportComponent = (
+				<GeneralReportByCustomer
+					state={state}
+					setState={setState}
+					data={data}
+				/>
+			);
+		}
+		if (groupBy === "worker") {
+			generalReportComponent = (
+				<GeneralReportByWorker state={state} setState={setState} data={data} />
+			);
+		}
+		if (groupBy === "date") {
+			generalReportComponent = (
+				<GeneralReportByDate state={state} setState={setState} data={data} />
+			);
+		}
 	}
+
+	return (
+		<Container>
+			<Title
+				text={`General report by ${groupBy === "worker" ? "cleaner" : groupBy}`}
+			/>
+			<h3 className="text-center mt-4 mt-md-5 mb-5 mb-md-5">
+				Work duration from {start_date} to {finish_date}
+			</h3>
+			{data.generalData.length === 0 ? (
+				<p>No data for this period.</p>
+			) : (
+				generalReportComponent
+			)}
+			<div className="d-flex justify-content-end mt-5">
+				<BackButton state={state} setState={setState} />
+			</div>
+		</Container>
+	);
 };
 
 GeneralReportResultPage.propTypes = {
